4b5674a85784527289fbd46311bff9ed4ccbb0d6
Bartosz Dusza <bartoszdusza7@gmail.com>
2023-02-22 19:19:31 +0100

    Commit 3

--- tutorial.py
+++ tutorial.py
@@ -9,18 +9,34 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5 #spped
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def get_backround():
-    pass
+#name is color of bg
+def get_backround(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+#add 1 so there is no gaps
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height +1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+    pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_backround("Blue.png")
 
     run = True
     while run:
@@ -30,6 +46,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+    draw(window, background, bg_image)
 
     pygame.quit()
     quit()
